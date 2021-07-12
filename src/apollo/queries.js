47e8074b15fbfa564794f50b460de78214f18fad
daoswap002 <daoswap@daoswap.cc>
2021-07-12 11:19:40 +0800

    modify subgraphName

--- src/apollo/queries.js
+++ src/apollo/queries.js
@@ -3,7 +3,7 @@ import { FACTORY_ADDRESS, BUNDLE_ID } from '../constants'
 
 export const SUBGRAPH_HEALTH = gql`
   query health {
-    indexingStatusForCurrentVersion(subgraphName: "daoswap-heco-mainnet/heco") {
+    indexingStatusForCurrentVersion(subgraphName: "daoswapdex-heco-mainnet/heco") {
       synced
       health
       chains {
